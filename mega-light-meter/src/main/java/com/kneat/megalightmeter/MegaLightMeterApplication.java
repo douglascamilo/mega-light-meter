9876ece0267053919cb63c152f9d03b0e7fbe9d5
Douglas Sousa <douglascamilo@gmail.com>
2020-01-22 02:21:05 -0300

    Initial commit

---
+++ mega-light-meter/src/main/java/com/kneat/megalightmeter/MegaLightMeterApplication.java
@@ -0,0 +1,15 @@
+package com.kneat.megalightmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application startup point
+ */
+@SpringBootApplication
+public class MegaLightMeterApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(MegaLightMeterApplication.class, args);
+	}
+}
---
+++ mega-light-meter/src/test/java/com/kneat/megalightmeter/MegaLightMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kneat.megalightmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaLightMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
