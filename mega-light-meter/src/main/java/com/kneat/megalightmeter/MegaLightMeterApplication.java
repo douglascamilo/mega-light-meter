4d09da46a639d46fc77d41aef6f103077ff36d49
Douglas Sousa <douglascamilo@gmail.com>
2020-01-26 22:33:29 -0300

    Creation of the KneatCommandLineRunner

--- mega-light-meter/src/main/java/com/kneat/megalightmeter/MegaLightMeterApplication.java
+++ mega-light-meter/src/main/java/com/kneat/megalightmeter/MegaLightMeterApplication.java
@@ -1,20 +1,11 @@
 package com.kneat.megalightmeter;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
-
-import com.kneat.megalightmeter.facade.CalculateStopsFacade;
-import com.kneat.megalightmeter.model.CalculateStopsResponse;
 
 /**
  * Application startup point
@@ -22,7 +13,6 @@ import com.kneat.megalightmeter.model.CalculateStopsResponse;
 @SpringBootApplication
 @EnableCaching
 public class MegaLightMeterApplication {
-	private static final Logger LOGGER = LoggerFactory.getLogger(MegaLightMeterApplication.class);
 
 	public static void main(final String[] args) {
 		SpringApplication.run(MegaLightMeterApplication.class, args);
@@ -31,27 +21,5 @@ public class MegaLightMeterApplication {
  	@Bean
  	public RestTemplate restTemplate(final RestTemplateBuilder builder) {
  		return builder.build();
- 	}
-
- 	@Bean
- 	public CommandLineRunner run(
- 			final CalculateStopsFacade calculateStopsFacade,
- 			final Environment environment) {
-
- 		return args -> {
- 			if (args != null && args.length == 1) {
- 				LOGGER.info("Application running on standalone mode...");
-
- 				final Long givenDistance = Long.parseLong(args[0]);
- 				final List<CalculateStopsResponse> response =
- 						calculateStopsFacade.getAllNeededStopsByStarShip(givenDistance);
-
- 				LOGGER.info(response.toString());
- 				System.exit(0);
- 			}
-
- 			LOGGER.info("Application running on http mode...");
- 			LOGGER.info("Endpoint: http://localhost:" + environment.getProperty("server.port") + "/calculate-stops/");
- 		};
  	}
 }
